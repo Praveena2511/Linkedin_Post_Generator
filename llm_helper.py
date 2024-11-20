5ae93e989e8c10f13c23d698d719dceef6126d99
Praveena2511 <praveenamuthukumar2511@gmail.com>
2024-11-20 18:49:25 +0530

    updated

--- llm_helper.py
+++ llm_helper.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-llm = ChatGroq(groq_api_key="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", model_name="llama-3.2-90b-text-preview")
+llm = ChatGroq(groq_api_key=os.getenv("groq_api"), model_name="llama-3.2-90b-text-preview")
 
 
 if __name__ == "__main__":
